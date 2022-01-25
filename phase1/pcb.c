aeaa28da98c7a14065fc65344819f5680f6c1f87
TheMancio <00lm@protonmail.ch>
2022-01-25 02:03:56 +0100

    Add: freePcb(), allocPcb()

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -8,11 +8,34 @@ void initPcbs() {
 }
 
 void freePcb(pcb_t* p) {
-
+    if (p != NULL) list_add_tail(&(p->p_list), &pcbFree_h); /* Aggiunge p a pcbFree_h */
 }
 
 pcb_t* allocPcb() {
+    if (list_empty(&pcbFree_h)) return NULL;                            /* Controllo lista vuota */
+    else {
+        pcb_PTR newPcb = container_of(pcbFree_h.next, pcb_t, p_list);   /* Viene trovato il primo PCB libero (successore dell'elemento sentinella) */
+        list_del(pcbFree_h.next);                                       /* Rimozione del successore della sentinella */
 
+        INIT_LIST_HEAD(&(newPcb->p_list));                              /* Inizializzazione delle liste */
+        INIT_LIST_HEAD(&(newPcb->p_child));
+        INIT_LIST_HEAD(&(newPcb->p_sib));
+
+        newPcb->p_parent = NULL;                                        /* Inizializzazione del genitore e semaforo */
+        newPcb->p_semAdd = NULL;
+
+        (newPcb->p_s).entry_hi = 0;                                     /* Inizializzazione della struct p_s (viene utilizzato un ciclo per il campo gpr) */
+        (newPcb->p_s).cause = 0;
+        (newPcb->p_s).status = 0;
+        (newPcb->p_s).pc_epc = 0;
+        (newPcb->p_s).hi = 0;
+        (newPcb->p_s).lo = 0;
+        for (int i = 0; i < STATE_GPR_LEN; i++) (newPcb->p_s).gpr[i] = 0;
+
+        newPcb->p_time = 0;
+
+        return newPcb;
+    }
 }
 
 void mkEmptyProcQ(struct list_head* head) {
