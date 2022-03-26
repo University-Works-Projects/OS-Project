945cf856c6673150a3188a9fedcd7dcf7cd4ed3e
jortikcode <jjuanjaramillo01@gmail.com>
2022-03-26 12:25:02 +0100

    Start&Stop: started and finished create_process

--- pandos/h/exceptions.h
+++ pandos/h/exceptions.h
@@ -4,9 +4,18 @@
 #include "./types.h"
 #include "./pandos_const.h"
 #include "./pandos_types.h"
+#include "./pcb.h"
+
 
 /* Stato del processore al momento dell'eccezione */
 state_t *exception_state; 
+
+/* Processo responsabile dell'eccezione */
+extern pcb_PTR current_p; 
+
+/* Ready queues */
+extern pcb_PTR ready_hq; 
+extern pcb_PTR ready_lq; 
 
 /* Gestore delle eccezioni */
 void exception_handler(); 
@@ -19,5 +28,16 @@ void tlb_handler();
 
 /* Gestore delle trap */
 void trap_handler(); 
+
+
+/* 
+    Syscalls 
+*/
+
+/* NSYS1 */
+void create_process(state_t *a1, int p_prio, support_t *p_support_struct); 
+
+/* NSYS2 */
+
 
 #endif
--- pandos/phase2/exceptions.c
+++ pandos/phase2/exceptions.c
@@ -37,11 +37,16 @@ void exception_handler(){
 void syscall_handler(){
     /* Intero che rappresenta il tipo di system call */
     int syscode = exception_state->reg_a0;
+
+    
     
     /* Switch per la gestione della syscall */
     switch(syscode){
         case CREATEPROCESS:
-            create_process(); 
+            state_t *a1 = (state_t *) exception_state->reg_a1; 
+            int p_prio = (int) exception_state->reg_a2; 
+            support_t *p_support_struct = (support_t *) exception_state->reg_a3; 
+            create_process(a1,p_prio,p_support_struct); 
             break; 
         case TERMPROCESS:
             break; 
@@ -65,7 +70,38 @@ void syscall_handler(){
 
 }
 
-void create_process(){
+void create_process(state_t *a1, int p_prio, support_t *p_support_struct){
+    /* Tentativo di allocazione di un nuovo processo */
+    pcb_PTR new_proc = allocPcb(); 
 
+    if (new_proc != NULL){                           /* Allocazione avvenuta con successo */
+        /* Il nuovo processo è figlio del processo chiamante */
+        insertChild(current_p,new_proc); 
+
+        /* Inizializzazione campi del nuovo processo a partire da a1,a2,a3 */
+        new_proc->p_s = *a1; 
+        new_proc->p_prio = p_prio; 
+        new_proc->p_supportStruct = p_support_struct; 
+
+        /* PID è implementato come l'indirizzo del pcb_t */
+        new_proc->p_pid = new_proc; 
+
+
+        /* Il nuovo processo è pronto per essere messo nella ready_q */
+        switch(new_proc->p_prio){
+            case 0:                                 /* E' un processo a bassa priorità */
+                insertProcQ(&(ready_lq->p_list),new_proc); 
+                break;
+            case 1:                                 /* E' un processo ad alta priorità */
+                insertProcQ(&(ready_hq->p_list),new_proc); 
+                break; 
+        }
+
+        /* Operazione completata, ritorno con successo */
+        current_p->p_s.reg_v0 = new_proc->p_pid; 
+    }else                                           /* Allocazione fallita, risorse non disponibili */
+        current_p->p_s.reg_v0 = NOPROC; 
+
+    //TODO: Leggere e implementare sezione 3.5.12 manuale pandosplus per il "return from a syscall"
 }
 
