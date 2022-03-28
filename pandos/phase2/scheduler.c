33a71c5575835c3ed8632f8667bf804994fdcd82
jortikcode <jjuanjaramillo01@gmail.com>
2022-03-28 22:35:19 +0200

    Add: added cpu time usage of processes

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -1,17 +1,26 @@
 #include "../h/scheduler.h"
 
+cpu_t start_usage_cpu;
+
 void scheduler() {
     pcb_PTR HP_pcb = headProcQ(&(ready_hq->p_list));            /* Ready High-Priority pcb list  */
     pcb_PTR LP_pcb = headProcQ(&(ready_lq->p_list));            /* Ready Low-Priority pcb list */
-    
+
+    /* Aggiornamento del tempo di uso della CPU: CURRENT_TOD - START_USAGE_TOD ; sezione 3.8 del manuale di pandosplus*/
+    if(current_p != NULL) current_p->p_time += ((*((cpu_t *) TODLOADDR)) / (*((cpu_t *) TIMESCALEADDR))) - start_usage_cpu; 
+
     if (HP_pcb != NULL) {                                       /* Se ci sono dei pcb_h Ready */
         /* Aggiornamento del processo attuale */
         current_p = removeProcQ(&(ready_hq->p_list));
         LDST(&(current_p->p_s));
+        /* Tempo di inizio di uso della CPU */
+        STCK(start_usage_cpu); 
     } else if (HP_pcb == NULL && LP_pcb != NULL){               /* Se vi sono solo pcb_l */
         current_p = removeProcQ(&(ready_lq->p_list));
         setTIMER(TIMESLICE);
         LDST(&(current_p->p_s));
+        /* Tempo di inizio di uso della CPU */
+        STCK(start_usage_cpu); 
     } else if (HP_pcb == NULL && LP_pcb == NULL) {              /* Se non vi sono precessi ad {alta & bassa} priorità */
         if (p_count == 0)
             HALT();
