24f9f726fad408f46986cc40aef742cb3feaf26d
jortikcode <jjuanjaramillo01@gmail.com>
2022-04-01 16:25:09 +0200

    Add: device interrupt handlers func. prototypes

--- pandos/h/interrupts.h
+++ pandos/h/interrupts.h
@@ -6,4 +6,27 @@
 
 void interrupt_handler(state_t* exception_state); 
 
+/* Interrupt Handlers */
+
+/* I.H for disk devices */
+void disk_interrupt_handler(int ip); 
+
+/* I.H for flash devices */
+void flash_interrupt_handler(int ip); 
+
+/* I.H for network adapters*/
+void network_interrupt_handler(int ip); 
+
+/* I.H for printer devices */
+void printer_interrupt_handler(int ip); 
+
+/* I.H for terminal devices */
+void terminal_interrupt_handler(int ip); 
+
+/* 
+    Acknowledge function for I/O interrupts. 
+    This function performs an ack on the dev_register and also a v on the appropriate semaphores.
+*/
+void acknowledge(int device_interrupting, int line, devreg_t *dev_register, int type); 
+
 #endif
--- pandos/phase2/interrupts.c
+++ pandos/phase2/interrupts.c
@@ -1,48 +1,43 @@
 #include "../h/interrupts.h"
 
 void interrupt_handler(state_t* exception_state){
-    /**
-     * Il nucleo deve gestire le linee di interrupt da 1 a 7.
-     * 
-     * Azioni che il nucleo deve svolgere:
-     *  1. Identificare la sorgente dell’interrupt
-     *      - Linea: registro Cause.IP
-     *      - Device sulla linea (>3): Interrupting Device Bit Map
-     *  2. Acknowledgment dell’interrupt
-     *      - Scrivere un comando di ack (linea >3) o un nuovo comando
-     *        nel registro del device.
-     * 
-     * Interrupt con numero di linea più bassa hanno priorità più alta, e
-     * dovrebbero essere gestiti per primi.
-     * 
-     * 
-     * The interrupt exception handler’s first step is to determine which
-     * device or timer with an outstanding interrupt is the highest priority.
-     * Depending on the device, the interrupt exception handler will perform a
-     * number of tasks.
-     */
-    
-    /* 
-        ip =     xxxxxxxx
-                 1 0000000 >> 8
-    */
+    /* Estrazione del campo IP dal registro CAUSE */
     int ip = exception_state->cause & IMON;
 
-    /**
-     * La priorità delle chiamate è implementata in base all'ordine di attivazione
-     * dei seguenti if.
-     */
+    /* La priorità delle chiamate è implementata in base all'ordine di attivazione dei seguenti if */
     if (ip && LOCALTIMERINT) {
-        
+    
     } else if (ip && TIMERINTERRUPT) {
 
     } else if (ip && DISKINTERRUPT) {
-        
+        disk_interrupt_handler(ip);     
     } else if (ip && FLASHINTERRUPT) { 
-        
+        flash_interrupt_handler(ip);        
+    } else if (ip && 0x00002000){
+        network_interrupt_handler(ip); 
     } else if (ip && PRINTINTERRUPT) { 
-        
+        printer_interrupt_handler(ip); 
     } else if (ip && TERMINTERRUPT) { 
-        
+        terminal_interrupt_handler(ip); 
     }
+}
+
+void disk_interrupt_handler(int ip){
+
+}
+
+void flash_interrupt_handler(int ip){
+
+}
+
+void network_interrupt_handler(int ip){
+
+}
+
+void printer_interrupt_handler(int ip){
+
+}
+
+void terminal_interrupt_handler(int ip){
+
 }
