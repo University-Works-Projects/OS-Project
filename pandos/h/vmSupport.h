f1c328c600f0b8ea8961e1df1a57c73302e6732d
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-07-25 11:53:37 +0200

    Start: pager function

--- pandos/h/vmSupport.h
+++ pandos/h/vmSupport.h
@@ -1,4 +1,10 @@
 #ifndef VMSUPPORT
 #define VMSUPPORT  
+#include <umps3/umps/libumps.h>
+#include "pandos_const.h"
+#include "pandos_types.h"
+
+/* Page fault exception handler */
+void pager(); 
 
 #endif
--- pandos/phase3/vmSupport.c
+++ pandos/phase3/vmSupport.c
@@ -1,1 +1,30 @@
 #include "../h/vmSupport.h"
+
+/* Swap pool mutex */
+int swap_pool_semaphore = 1; 
+/* Swap pool */
+swap_t swap_pool[POOLSIZE]; 
+
+extern pcb_PTR current_p; 
+
+void pager(){
+	// Recupero della struttura di supporto del processo corrente
+	support_t *curr_support = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0); 
+	// Estrazione del Cause.ExcCode
+	int cause = curr_support->sup_exceptState[0].cause & GETEXECCODE; 
+	cause >>= 2; 
+
+	if (cause == 1){
+		// TLB-Modification exception, si gestisce come una program trap
+	}
+	
+	// Acquisizione della mutua esclusione sulla swap pool table
+	SYSCALL(PASSEREN, &swap_pool_semaphore, 0, 0); 
+	
+	// Acquisizione del numero della pagina da caricare in memoria
+	int page_missing = curr_support->sup_exceptState[0].entry_hi >> VPNSHIFT; 
+
+	// Rilascio della mutua esclusione sulla swap pool table
+	SYSCALL(VERHOGEN, &swap_pool_semaphore, 0, 0); 
+
+}
