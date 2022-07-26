c09c4fd2b5c8ebb15db7b5c16fe76631dc288991
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-07-26 09:40:20 +0200

    Start: sysSupport

--- pandos/h/sysSupport.h
+++ pandos/h/sysSupport.h
@@ -1,6 +1,26 @@
 #ifndef SYSSUPPORT 
-#define SYSSUPPORT 
+#define SYSSUPPORT
 
+#include "../testers/h/tconst.h"    /* To use the constant values in the switch case in the .c file */
+#include "pandos_const.h"
+#include "pandos_types.h"
+
+void generalException_hanlder();
+
+/* NSYS11 */
+void get_tod ();
+
+/* NSYS12 */
+void terminate ();
+
+/* NSYS13 */
+void write_to_printer ();
+
+/* NSYS14 */
+void write_to_terminal ();
+
+/* NSYS15 */
+void read_from_terminal ();
 
 
 #endif
--- pandos/phase3/sysSupport.c
+++ pandos/phase3/sysSupport.c
@@ -1,1 +1,60 @@
 #include "../h/sysSupport.h"
+
+void generalException_hanlder() {
+
+    support_t* currProc_support = (support_t*) SYSCALL(GETSUPPORTPTR, 0, 0, 0);     /* SYS8 to get the supp struct's addrs of the curr proc */
+
+    /* [Section 4.8] and [Section 3.7.2] (and [Section 3.4]) */
+    // Why .[GENERALEXCEPT] ?
+    int exeCode = currProc_support->sup_exceptState[GENERALEXCEPT].cause;       /*  */
+    if (9 <= exeCode && exeCode >= 12)
+        terminate();
+
+    int syscode = currProc_support->sup_exceptState[GENERALEXCEPT].reg_a0;
+
+    switch (syscode) {
+        case GET_TOD: {
+            get_tod();
+        }
+        case TERMINATE: {
+            terminate();
+        }
+        case WRITEPRINTER: {
+            write_to_printer();
+        }
+        case WRITETERMINAL: {
+            write_to_terminal();
+        }
+        case READTERMINAL: {
+            read_from_terminal();
+        }
+        default: {
+
+        }
+    }
+
+}
+
+void get_tod () {
+    
+}
+
+void terminate () {
+   /**
+    * If the process to be terminated is currently holding mutual exclusion on
+    * a Support Level semaphore (e.g. Swap Pool semaphore), mutual exclusion must
+    * first be released (NSYS4) before invoking the Nucleus terminate command (NSYS2).
+    */
+}
+
+void write_to_printer () {
+    
+}
+
+void write_to_terminal () {
+    
+}
+
+void read_from_terminal () {
+    
+}
